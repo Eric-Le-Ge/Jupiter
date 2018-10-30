9ee20b33e8ddbb54121f1e579338a763ba963029
Eric Ge <ericgele@163.com>
2018-10-29 23:56:08 -0700

    frontend

--- Jupiter/src/rpc/SearchItem.java
+++ Jupiter/src/rpc/SearchItem.java
@@ -1,10 +1,7 @@
 package rpc;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,17 +9,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import entity.Item;
+import external.TicketMasterAPI;
+
 /**
- * Servlet implementation class RecommendItem
+ * Servlet implementation class SearchItem
  */
-@WebServlet("/recommendation")
-public class RecommendItem extends HttpServlet {
+@WebServlet("/search")
+public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public RecommendItem() {
+    public SearchItem() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,19 +35,25 @@ public class RecommendItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.setContentType("application/json");
-		PrintWriter out = response.getWriter();
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		
+		// term can be empty
+		String term= request.getParameter("term");
+		TicketMasterAPI tmAPI = new TicketMasterAPI();
+		List<Item> items = tmAPI.search(lat, lon, term);
+		
 		JSONArray array = new JSONArray();
-
 		try {
-			array.put(new JSONObject().put("username", "abcd").put("address", "San Francisco").put("time", "01/01/2017"));
-			array.put(new JSONObject().put("username", "1234").put("address", "San Jose").put("time", "01/02/2017"));
-		} catch (JSONException e) {
+			for (Item item : items) {
+				JSONObject obj = item.toJSONObject();
+				array.put(obj);
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 		RpcHelper.writeJsonArray(response, array);
+
 	}
 
 	/**
